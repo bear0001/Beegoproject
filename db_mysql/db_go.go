87ce5e6791b4f5184945fb3258746e727d5de72c
bell001 <930987934@qq.com>
2020-09-26 16:54:29 +0800

    9.25第二次提交

--- db_mysql/db_go.go
+++ db_mysql/db_go.go
@@ -4,23 +4,24 @@ import (
 	"database/sql"
 	"fmt"
 	"github.com/astaxie/beego"
+	_"github.com/go-sql-driver/mysql"
 )
 var Db*sql.DB
 
-func init()  {
+func init() {
+
 	fmt.Println("连接mysql数据库")
-	config :=beego.AppConfig
-	dbDriver :=config.String("db_riverName")
-	dbUser :=config.String("db_user")
-	dbPassword:=config.String("db_password")
-	dbIp:=config.String("db_ip")
-	dbName :=config.String("db_name")
+	config := beego.AppConfig
+	dbDriver := config.String("db_riverName")
+	dbUser := config.String("db_user")
+	dbPassword := config.String("db_password")
+	dbIp := config.String("db_ip")
+	dbName := config.String("db_name")
 
-	connUrl:=dbUser+":"+dbPassword+"@tcp("+dbIp+")/"+dbName+"?charset=utf8"
-	db,err:=sql.Open(dbDriver,connUrl)
-	if err !=nil{
+	connUrl := dbUser + ":" + dbPassword + "@tcp(" + dbIp + ")/" + dbName + "?charset=utf8"
+	db, err := sql.Open(dbDriver, connUrl)
+	if err != nil {
 		panic("数据库连接错误，请配置检查")
-    Db = db
+		Db = db
+	}
 }
-
-
--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	//_"BeegoProject0603(1)/BeegoProject0603/db_mysql"
 	_ "Project922/routers"
+	_ "database/sql"
 	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego"
 )
 
-func main() {
-
+func main()  {
 	beego.Run()
 }
+//var Db *sql.DB
+
+	//appName :=config.String("appname")
+	//fmt.Println("应用名称:",appName)
+	//port,err :=config.Int("httpport")
+	//if err!=nil {
+	//	panic("项目配置文件解析失败，请检查配置文件")
+	//}
+	//dbDirver :=config.String("db_driverName")
+	//dbUser:=config.String("db_user")
+	//dbPassword:=config.String("db_password")
+	//dbIp:=config.String("db_ip")
+	////dbPort:=config.String("db_port")
+	//dbName:=config.String("db_name")
+	//
+	//connUrl :=dbUser+":"+dbPassword+"@tcp("+dbIp+")/"+dbName+"charset=uft-8"
+	//fmt.Println(port)
+	//  db,err:=sql.Open(dbDirver,connUrl)
+	//
+	//beego.Run()
+
+
+//在初始化函数中链接数据库
+
 
